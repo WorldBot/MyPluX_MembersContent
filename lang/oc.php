4fe57447e3052d10dfcfa8b904a88273e4e10cd1
Quentí <33203663+Quenty31@users.noreply.github.com>
2018-05-14 20:40:24 +0200

    Create oc.php

---
+++ lang/oc.php
@@ -0,0 +1,21 @@
+<?php
+
+$LANG = array(
+
+'L_SAVE'                       => "Enregistrar",
+'L_BY_DEFAULT_ARTICLES'        => "Per defaut l\estatut dels articles es",
+'L_BY_DEFAULT_PAGES'           => " Per defaut l\estatut de las paginas es",
+'L_PUBLIC'                     => "Public",
+'L_MEMBERS'                    => "Reservat als membres",
+'L_BY_DEFAULT'                 => "Per defaut del Plugin",
+'L_ARTICLE_TXT_REPLACE'        => "Remplaçar le contengut per",
+'L_ARTICLE_CONTENT'            => "Aqueste article es reservat a nòstres membres.",
+'L_PAGE_TXT_REPLACE'           => "Remplaçar lo contengut per",
+'L_PAGE_CONTENT'               => "Aquesta pagina es reservada a nòstres membres.",
+'L_CHAPO_TXT_ADD_BEFORE'       => "Ajustar davant lo châpo",
+'L_CHAPO_TXT_ADD_AFTER'        => "Ajutar aprèp lo châpo",
+'L_NEVER'                      => "Ajustar pas res",
+'L_ALWAYS'                     => "Totjorn ajustar ( als membres e pas-connectats )",
+'L_ONLY'                       => "Ajustar solament als visitaires",
+
+);
